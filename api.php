8e8c2d037c9bc9ee7859f0b391db6a4be0371a4b
Jake Petty <jake.petty@vexcon.net>
2019-05-15 23:35:40 -0500

    Moved class to separate file

--- api.php
+++ api.php
@@ -1,30 +1,6 @@
 <?php
-class PoliceSearch
-{
-    private $headers = [
-        'Origin: http://apps.cedar-rapids.org',
-        'Host: apps.cedar-rapids.org',
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
-        'referer: http://apps.cedar-rapids.org/policelog',
-        'X-Requested-With: XMLHttpRequest'
-    ];
-
-    public function get($url, $data)
-    {
-        $ch = curl_init('http://apps.cedar-rapids.org' . $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, !isset($data['url']) ? $data : [
-            'X-Requested-With' => 'XMLHttpRequest'
-        ]);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-        $callback = curl_exec($ch);
-        curl_close($ch);
-
-        return $callback;
-    }
-}
-if (!empty($_POST)) {
+require "classes/police.class.php";
+if (isset($_POST["data"])) {
     $search = new PoliceSearch();
 
     // Decode Base64 data
@@ -36,5 +12,5 @@ if (!empty($_POST)) {
     // Get results
     echo $search->get(isset($data['url']) ? $data['url'] : "/PoliceLog/Home/Search", $data);
 } else {
-    echo "Invalid Request";
+    echo "Access Denied";
 }
